f1af9cba9f5fa8cde3718e2a088a89e139c7f416
Johann Moreno Noda <johannmorenonoda@proton.me>
2026-05-11 21:42:45 +0100

    chore: remove stray local helper accidentally committed

--- _set-infra-pat.ts
+++
@@ -1,5 +0,0 @@
-import { GitHubService } from './src/services/github.service';
-const svc = new GitHubService();
-svc.createRepoSecret('Johanson1988', 'bbdd-agent', 'INFRA_PAT', process.env.GITHUB_TOKEN!)
-  .then(() => { console.log('OK - INFRA_PAT created'); process.exit(0); })
-  .catch((e: any) => { console.error('ERROR:', e.message); process.exit(1); });
